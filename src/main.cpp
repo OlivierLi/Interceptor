17b0b5f92d74fa285703491afb25fea4a140edbb
Olivier Li <OlivierLiDupuis@gmail.com>
2014-05-02 10:52:17 -0400

    Move things around and start adding necessary util functions

--- src/main.cpp
+++ src/main.cpp
@@ -11,13 +11,36 @@
 std::chrono::steady_clock::time_point start;
 std::chrono::steady_clock::time_point end;
 
-int main() {
+//-----------------------------------------------------
+// SFML setup
+//-----------------------------------------------------
+sf::Window window(sf::VideoMode(1280, 720), "OpenGL", sf::Style::Close);
 
-    //-----------------------------------------------------
-    // SFML setup
-    //-----------------------------------------------------
-    sf::Window window(sf::VideoMode(1280, 720), "OpenGL", sf::Style::Close);
+bool running = true;
+
+void process_input(){
+
     sf::Event windowEvent;
+
+    while (window.pollEvent(windowEvent)) {
+        switch (windowEvent.type) {
+            case sf::Event::Closed:
+                running = false;
+                break;
+        }
+    }
+
+}
+
+void update(){
+
+}
+
+void render(){
+    window.display();
+}
+
+int main() {
 
     //-----------------------------------------------------
     // Glew setup
@@ -28,21 +51,16 @@ int main() {
     //-----------------------------------------------------
     // Main game loop
     //-----------------------------------------------------
-    bool running = true;
     while (running) {
 
+        //Get the time at the beginning of the frame
         start = std::chrono::steady_clock::now();
 
-        while (window.pollEvent(windowEvent)) {
-            switch (windowEvent.type) {
-            case sf::Event::Closed:
-                running = false;
-                break;
-            }
-        }
+        process_input();
+        update();
+        render();
 
-        window.display();
-
+        //Calculate how long the frame took to process
         end = std::chrono::steady_clock::now();
         long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(end-start).count();
 
---
+++ src/util.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+std::string read_shader_file(std::string shader_file_name){
+
+    std::ifstream stream(shader_file_name);
+
+    if(!stream)
+    {
+        std::cout<< shader_file_name << " file not found." << std::endl;
+        exit(1);
+    }
+
+    std::stringstream buffer;
+    buffer << stream.rdbuf();
+
+    std::string program_source = buffer.str();
+
+    if(program_source.length()==0)
+    {
+        std::cout<< shader_file_name << " is empty." << std::endl;
+        exit(1);
+    }
+
+    return program_source;
+}
