1217ae820f98428930fee467d18b44c1f51b6604
Olivier Li <OlivierLiDupuis@gmail.com>
2014-08-14 22:08:48 -0400

    Better shader compiling

--- src/Rendering/renderer.cpp
+++ src/Rendering/renderer.cpp
@@ -8,46 +8,29 @@ Renderer::Renderer()
     glewExperimental = GL_TRUE;
     glewInit();
 
-    //-----------------------------------------------------
-    // Create programs
-    //-----------------------------------------------------
-    GLint status;
+    //Create programs
+    create_program("shaders/triangles.vert","shaders/triangles.frag","triangles");
+}
+
+void Renderer::create_program(std::string vertex_path, std::string fragment_path ,std::string program_name){
 
     // Create and compile the vertex shader ------------------------------------------
-    const char *vertexSource = read_shader_file("shaders/triangles.vert").c_str();
+    const char *vertexSource = read_shader_file(vertex_path).c_str();
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexSource, NULL);
     glCompileShader(vertexShader);
-
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
-    if(status != GL_TRUE){
-
-        char buffer[512];
-        glGetShaderInfoLog(vertexShader, 512, NULL, buffer);
-        std::cout << buffer << std::endl;
-
-        throw std::runtime_error("Vertex shader compilation failed!");
-    }
+    test_shader_compilation(vertexShader);
 
     shaders.push_back(vertexShader);
-
     //--------------------------------------------------------------------------------
 
     // Create and compile the fragment shader ----------------------------------------
-    const char *fragmentSource = read_shader_file("shaders/triangles.frag").c_str();
+    const char *fragmentSource = read_shader_file(fragment_path).c_str();
     GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
     glCompileShader(fragmentShader);
+    test_shader_compilation(fragmentShader);
 
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status);
-    if(status != GL_TRUE){
-
-        char buffer[512];
-        glGetShaderInfoLog(fragmentShader, 512, NULL, buffer);
-        std::cout << buffer << std::endl;
-
-        throw std::runtime_error("Fragment shader compilation failed!");
-    }
     shaders.push_back(fragmentShader);
     //--------------------------------------------------------------------------------
 
@@ -57,12 +40,26 @@ Renderer::Renderer()
     glAttachShader(shaderProgram, fragmentShader);
     glBindFragDataLocation(shaderProgram, 0, "outColor");
     glLinkProgram(shaderProgram);
+
     programs.push_back(shaderProgram);
+    programs_map.insert(std::pair<std::string,GLuint>(program_name,shaderProgram));
     //--------------------------------------------------------------------------------
 
+    //Set the format of buffers for now. Later we will use different formats
+}
 
-    //TODO also keep track of programs in a Map
+void Renderer::test_shader_compilation(GLuint shader){
+    GLint status;
 
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+    if(status != GL_TRUE){
+
+        char buffer[512];
+        glGetShaderInfoLog(shader, 512, NULL, buffer);
+        std::cout << buffer << std::endl;
+
+        throw std::runtime_error("Fragment shader compilation failed!");
+    }
 }
 
 Renderer::~Renderer(){
--- src/Rendering/renderer.h
+++ src/Rendering/renderer.h
@@ -6,6 +6,7 @@
 
 #include "../util.hpp"
 #include <vector>
+#include <map>
 
 class Renderer
 {
@@ -14,10 +15,19 @@ public:
     ~Renderer();
     void clear();
 private:
+
+    //Used to keep track of all ressources to dealocate
     std::vector<GLuint> shaders;
     std::vector<GLuint> programs;
     std::vector<GLuint> buffers;
     std::vector<GLuint> vertex_arrays;
+
+    //Use to select programs
+    std::map<std::string,GLuint> programs_map;
+
+    void create_program(std::string vertex_path, std::string fragment_path ,std::string program_name);
+    void test_shader_compilation(GLuint shader);
+
 };
 
 #endif // RENDERER_H
