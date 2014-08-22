fc2f1868f7cd68145e0b341c26f07ee59dc34207
Olivier Li <OlivierLiDupuis@gmail.com>
2014-08-21 22:20:36 -0400

    Output hollow shapes using lines

--- shaders/enemies_geo.glsl
+++ shaders/enemies_geo.glsl
@@ -1,7 +1,7 @@
 #version 440 core
 
 layout(points) in;
-layout(triangle_strip, max_vertices = 3) out;
+layout(line_strip, max_vertices = 4) out;
 
 void main() {
 
@@ -12,6 +12,9 @@ void main() {
     EmitVertex();
 
     gl_Position = gl_in[0].gl_Position + vec4(0.0, -0.04, 0.0, 0.0);
+    EmitVertex();
+
+    gl_Position = gl_in[0].gl_Position;
     EmitVertex();
 
     EndPrimitive();
